3e8861e0248e0c31cf0cfaa5bb847b76ae72b544
milescm <milescm@nate.com>
2020-07-27 10:26:16 +0900

    Add urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # call blog urls This method send all requests to 127.0.0.1:8000
+    path('', include('blog.urls')),
 ]
+
